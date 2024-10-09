c3c4e9e57d15759b2381ca7758ee8d1e382bfec7
Héctor Rivas <hectorrivasexposito@gmail.com>
2024-10-09 13:26:03 +0200

    commit

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const user = require(__dirname + "/routes/users");
 dotenv.config();
 
 //Conexión a la base de datos MongoDB
+//esperemos que funcione
 mongoose.connect(process.env.DATABASE_URL)
     .then(() => {
         console.log("Conectado a la base de datos");
